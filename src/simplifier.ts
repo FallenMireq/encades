7f02138f7d710524b5c9d7d03933379dca3325ec
Fallen Mireq <shipiguzov@tados.ru>
2018-11-08 16:59:36 +0500

    debug changes...

--- src/simplifier.ts
+++ src/simplifier.ts
@@ -2,34 +2,37 @@ import * as CAdES from 'cadesplugin-types';
 import * as Simple from './simplifier/@';
 
 async function run() {
+    // configuration
+
     let simpleCades = new Simple.SimpleCades();
 
-    simpleCades.SignCadesOptions.include = CAdES.CAPICOM_CERTIFICATE_INCLUDE_OPTION.CAPICOM_CERTIFICATE_INCLUDE_WHOLE_CHAIN;
+    simpleCades.SignCadesOptions.include =
+        CAdES.CAPICOM_CERTIFICATE_INCLUDE_OPTION.CAPICOM_CERTIFICATE_INCLUDE_WHOLE_CHAIN;
     simpleCades.SignCadesOptions.cadesType = CAdES.CADESCOM_CADES_TYPE.CADESCOM_CADES_BES;
     simpleCades.SignCadesOptions.detached = true;
 
-
     // Getting certificate list
 
-    let certificates = await simpleCades.getCertificates([
-        {
-            type: CAdES.CAPICOM_CERTIFICATE_FIND_TYPE.CAPICOM_CERTIFICATE_FIND_SUBJECT_NAME,
-            criteria: 'TestName',
-        },
-    ]);
+    let certificates = await simpleCades.getCertificates([{
+        type: CAdES.CAPICOM_CERTIFICATE_FIND_TYPE.CAPICOM_CERTIFICATE_FIND_KEY_USAGE,
+        criteria: CAdES.CAPICOM_KEY_USAGE.CAPICOM_DIGITAL_SIGNATURE_KEY_USAGE,
+    }, {
+        type: CAdES.CAPICOM_CERTIFICATE_FIND_TYPE.CAPICOM_CERTIFICATE_FIND_TIME_VALID,
+    }]);
 
-    if (certificates.length !== 1) {
+    if (certificates.length === 0) {
         console.log('Found %d sertificate(s).', certificates.length);
         return;
     }
 
-    console.log(certificates[0]);
+    let certificate = certificates[0];
+    console.log(certificate);
 
-    // signing
+    // Signing
 
     let date = new Date(2017, 7, 7, 2, 15, 0, 0);
 
-    let signature = await simpleCades.signCades('Hello, world!', certificates[0], [
+    let signature = await simpleCades.signCades('Hello, world!', certificate, [
         new Simple.SimpleSigningTimeAttribute(date),
     ]);
 
